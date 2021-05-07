e986252b2f895fd4ca5a568124d4518871c100e7
zenas <zenas@pop-os.localdomain>
2021-05-07 18:06:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import AllPokemon from './containers/AllPokemon';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="text-center text-3xl m-10">
+      Personal Pokedex
+      <div class="text-2xl m-10">
+        <AllPokemon />
+      </div>
     </div>
   );
-}
+    
+};
 
 export default App;
